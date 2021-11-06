3924428c12002bdfc36ca482e96b5fc4124b8b07
AbdullahMakhdoom <abdullahmakhdoom1998@gmail.com>
2021-11-06 18:12:44 +0500

    T20 score predictor web app

---
+++ app.py
@@ -0,0 +1,95 @@
+from numpy.lib.histograms import _histogram_bin_edges_dispatcher
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+model = pickle.load(open('automl_model.pkl', 'rb'))
+
+teams = ['Australia',
+ 'India',
+ 'Bangladesh',
+ 'New Zealand',
+ 'South Africa',
+ 'England',
+ 'West Indies',
+ 'Afghanistan',
+ 'Pakistan',
+ 'Sri Lanka']
+
+cities = ['Colombo',
+ 'Mirpur',
+ 'Johannesburg',
+ 'Dubai',
+ 'Auckland',
+ 'Cape Town',
+ 'London',
+ 'Pallekele',
+ 'Barbados',
+ 'Sydney',
+ 'Melbourne',
+ 'Durban',
+ 'St Lucia',
+ 'Wellington',
+ 'Lauderhill',
+ 'Hamilton',
+ 'Centurion',
+ 'Manchester',
+ 'Abu Dhabi',
+ 'Mumbai',
+ 'Nottingham',
+ 'Southampton',
+ 'Mount Maunganui',
+ 'Chittagong',
+ 'Kolkata',
+ 'Lahore',
+ 'Delhi',
+ 'Nagpur',
+ 'Chandigarh',
+ 'Adelaide',
+ 'Bangalore',
+ 'St Kitts',
+ 'Cardiff',
+ 'Christchurch',
+ 'Trinidad']
+
+st.title('Cricket Score Predictor for 1st innings')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select batting team', sorted(teams))
+
+with col2:
+    bowling_team = st.selectbox('Select bowling team', sorted(teams))
+
+city = st.selectbox('Select city', sorted(cities))
+
+col3, col4, col5 = st.columns(3)
+
+with col3:
+    current_score = st.number_input('Current Score')
+
+with col4:
+    overs = st.number_input('Overs done')
+
+with col5:
+    wickets = st.number_input('Wickets out')
+
+if st.button('Predict Runs'):
+    balls_left = 120 - (overs * 6)
+    wickets_left = 10 - wickets
+    crr = current_score / overs
+
+    input_df = pd.DataFrame(
+        {'batting_team': [batting_team], 
+        'bowling_team': [bowling_team],
+        'city': city,
+        'current_score': [current_score],
+        'balls_left': [balls_left],
+        'wickets_left':[wickets],
+        'crr': [crr]})
+
+    result = model.predict(input_df)
+
+    st.header("Predicted Score - " + str(int(result[0])))
